3c3a3fcc4e61f1056ed89ed96c7e931dbf99412d
kanaktyagi <tyagiknk.india@gmail.com>
2022-01-05 19:47:40 +0530

    Added the ui for practice project

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import classes from './Modal.module.css'
+import  ReactDOM from 'react-dom'
+
+const Backdrop = props => {
+    return <div className={classes.backdrop} onClick={props.onClose}/>
+
+}
+
+const ModalOverLay = props => {
+    return <div className={classes.modal}>
+    <div className={classes.content}>{props.children} </div>
+    </div>
+}
+ const portalElement = document.getElementById('overlays')
+function Modal(props) {
+    return (
+        <>
+        {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>,portalElement) }
+        {ReactDOM.createPortal(<ModalOverLay>{props.children}</ModalOverLay>, portalElement)}
+            
+        </>
+    )
+}
+
+export default Modal
