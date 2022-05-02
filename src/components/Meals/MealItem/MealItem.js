eb0979ba72a192d39bbdbb929fd2e374b8569452
kanaktyagi <tyagiknk.india@gmail.com>
2022-05-02 18:57:59 +0530

    Fixed the positioning of the meals

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -19,9 +19,11 @@ function MealItem(props) {
     }
     return (
        <li className={classes.meal}>
+       <div>
        <div> <h3>{props.name} </h3></div>
        <div className={classes.description}>{props.description}</div>
        <div className={classes.price}>{price}</div>
+       </div>
        <div> <MealItemForm id={props.id} onAddToCart={addToCartHandler}/></div>
        </li>
     )
